ddbdacdf046b4abb60805f6a7b09c42dbcd81692
mishakuzma <mishakuzma@gmail.com>
2016-09-03 22:09:56 -0400

    Added structure for backgroundImgChange function
    - new backgroundImgChange.js file added
    - Contains function that accepts search terms for img related to quote,
    uses api call to stock image website, and changes the background image
    to be related to entered search terms.

---
+++ backgroundImgChange.js
@@ -0,0 +1,7 @@
+// Function that changes the background image depending on the content of the quote
+ // -Accepts string containing key terms for searching for image,
+ //     and changes background image based on the image that's found.
+
+function backgroundImgChange(searchStr){
+
+};
